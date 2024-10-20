eb9eab98354632bac9dab1ec67088ade1a7f8853
gellenee <glennedelatorre@gmail.com>
2024-10-19 20:59:49 -0400

    update question1

--- iOS-Hackathon-2024/iOS Hackathon 2024/Question1.swift
+++ iOS-Hackathon-2024/iOS Hackathon 2024/Question1.swift
@@ -1,1 +1,32 @@
+import SwiftUI
+struct Question1: View{
+    var feelings: [String: String]
+    @State var newEmotion = ""
+    var body: some View {
+        VStack {
+            
+            Text("How are you feeling right now?")
+            Button("Shining"){
+                newEmotion = "Shining"
+            }
+            Button("Peaceful"){
+                newEmotion = "Peaceful"
+            }
+            Button("Dark"){
+                newEmotion = "Dark"
+            }
+            .padding()
+            Button("Next"){
+                let date = Date()
+                feelings[date] = newEmotion
+                
+            }
+        }
+        .padding()
+    }
+}
+
+#Preview{
+    Question1(feelings: ["String": "String"] )
+}
 
